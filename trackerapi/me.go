74cedb4343aa4f56fe2f5fbf2f44f9359b66ec38
Prasad Surase <prasadsurase@gmail.com>
2018-01-08 15:30:42 +0530

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/prasadsurase/clirescue/cmdutil"
+	"github.com/prasadsurase/clirescue/user"
 )
 
 var (
